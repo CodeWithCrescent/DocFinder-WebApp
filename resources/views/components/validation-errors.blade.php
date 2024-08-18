4d91138055e94e3d56fe53814beb40301b41acf5
Crescent Geniius <crescentbeatz31@gmail.com>
2024-08-18 03:36:12 +0300

    Removed files

--- resources/views/components/validation-errors.blade.php
+++
@@ -1,9 +0,0 @@
-@if ($errors->any())
-        <div class="text-danger">{{ __('Whoops! Something went wrong.') }}</div>
-
-        <ul class="mt-3 text-danger">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-@endif
