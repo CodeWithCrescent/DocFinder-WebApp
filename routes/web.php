611370e933a4073ffb5b3db83fcaae133fff1b0a
Crescent Geniius <crescentbeatz31@gmail.com>
2024-08-18 03:29:17 +0300

    updated the app routes

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,14 @@ Route::prefix('/admin')->middleware([
     'verified',
 ])->group(function () {
     Route::get('/dashboard', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.dashboard');
+    Route::get('/hospitals', [App\Http\Controllers\HospitalController::class, 'showHospitals'])->name('admin.hospitals');
+    Route::post('/hospitals/create', [App\Http\Controllers\HospitalController::class, 'createHospital'])->name('admin.hospital.create');
+    Route::get('/hospitals/{id}/edit', [App\Http\Controllers\HospitalController::class, 'editHospital'])->name('admin.hospital.edit');
+    Route::put('/hospitals/{id}/update', [App\Http\Controllers\HospitalController::class, 'updateHospital'])->name('admin.hospital.update');
+    Route::post('/hospitals/department/create', [App\Http\Controllers\HospitalController::class, 'createHospitalDepartment'])->name('admin.department.create');
     Route::get('/appointments', [App\Http\Controllers\AdminController::class, 'showAppointments'])->name('admin.appointments');
     Route::get('/doctors', [App\Http\Controllers\AdminController::class, 'showDoctors'])->name('admin.doctors');
     Route::get('/add-doctor', [App\Http\Controllers\AdminController::class, 'addDoctor'])->name('admin.add.doctor');
+    Route::post('/add-doctor', [App\Http\Controllers\AdminController::class, 'addDoctorAction'])->name('admin.add.doctor');
     Route::get('/patients', [App\Http\Controllers\AdminController::class, 'showPatients'])->name('admin.patients');
 });
