7938365a599f86b425a6765f10ed3831df26a906
Crescent Geniius <crescentbeatz31@gmail.com>
2024-08-18 03:25:59 +0300

    Updated the password resert page

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
             <x-authentication-card-logo />
@@ -33,4 +33,46 @@
             </div>
         </form>
     </x-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+
+@extends('layouts.guest')
+@section('auth-form')
+    <form method="POST" action="{{ route('password.update') }}" class="login-form mt-4">
+        @csrf
+
+        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="mb-3">
+                    <label class="form-label">{{ __('Email') }} <span class="text-danger">*</span></label>
+                    <input type="email" class="form-control" name="email" value="{{ old('email', $request->email) }}" required autofocus
+                        autocomplete="username">
+                </div>
+            </div>
+
+            <div class="col-lg-12">
+                <div class="mb-3">
+                    <label class="form-label">{{ __('Password') }} <span class="text-danger">*</span></label>
+                    <input type="password" class="form-control" name="password" required autocomplete="current-password">
+                </div>
+            </div>
+
+            <div class="col-md-12">
+                <div class="mb-3">
+                    <label class="form-label">{{ __('Confirm Password') }} <span class="text-danger">*</span></label>
+                    <input type="password" class="form-control" name="password_confirmation" required
+                        autocomplete="new-password">
+                </div>
+            </div>
+
+            <div class="col-lg-12 mb-0">
+                <div class="d-grid">
+                    <button type="submit" class="btn btn-primary">{{ __('Reset Password') }}</button>
+                </div>
+            </div>
+
+        </div>
+    </form>
+@endsection
