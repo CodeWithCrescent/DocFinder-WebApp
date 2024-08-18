e9065265cf71067e41e8bb25842b587f9ea92106
Crescent Geniius <crescentbeatz31@gmail.com>
2024-08-18 03:27:09 +0300

    Updated the doctor details model to add relationships

--- app/Models/DoctorDetails.php
+++ app/Models/DoctorDetails.php
@@ -4,8 +4,45 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DoctorDetails extends Model
 {
     use HasFactory;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'hospital_id',
+        'department_id',
+        'specialty',
+        'gender',
+        'started_year',
+        'bio',
+    ];
+
+
+    /**
+     * The attributes that are pre-defined.
+     *
+     * @var array<int, string>
+     */
+
+    protected $attributes = [
+        'specialty' => 'General',
+    ];
+
+    /**
+     * Get the user that owns the DoctorDetails
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
